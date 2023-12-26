dae2b427fc76804c27e171e943a5da7b0d6092d5
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-26 09:45:07 +0000

    Updating to latest FlutterFlow output.

--- lib/company_page/join_company_page/join_company_page_model.dart
+++ lib/company_page/join_company_page/join_company_page_model.dart
@@ -1,10 +1,12 @@
 import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
+import '/components/information_dialog_view_widget.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'join_company_page_widget.dart' show JoinCompanyPageWidget;
+import 'package:aligned_dialog/aligned_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
