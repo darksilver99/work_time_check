8c0456a3c9d79867ed7905e6ea5788abdad7939a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-07 10:51:56 +0000

    Updating to latest FlutterFlow output.

--- lib/member_page/login_page/login_page_model.dart
+++ lib/member_page/login_page/login_page_model.dart
@@ -3,7 +3,6 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'login_page_widget.dart' show LoginPageWidget;
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
