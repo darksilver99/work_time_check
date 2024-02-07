4de3fec4b5721fda4a993cd9e23b923f64e0ff07
Darksilver88 <anusorn@koder3.com>
2024-02-07 13:14:25 +0700

    a

--- lib/flutter_flow/custom_functions.dart
+++ lib/flutter_flow/custom_functions.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:timeago/timeago.dart' as timeago;
+import 'flutter_flow_util.dart';
 import 'lat_lng.dart';
 import 'place.dart';
 import 'uploaded_file.dart';
@@ -49,7 +50,7 @@ String? getThaiDatetime(
 }
 
 String? getThaiDayName(DateTime? date) {
-  int dayOfWeek = date.weekday;
+  int dayOfWeek = date!.weekday;
   List<String> dayName = [
     "อาทิตย์",
     "จันทร์",
