b6a99324a015d72af7a7e54697f7e4be56326e91
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-21 02:34:17 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/customer_view/customer_q_r_code_view/customer_q_r_code_view_model.dart
@@ -0,0 +1,20 @@
+import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'dart:math';
+import 'customer_q_r_code_view_widget.dart' show CustomerQRCodeViewWidget;
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class CustomerQRCodeViewModel
+    extends FlutterFlowModel<CustomerQRCodeViewWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
---
+++ lib/customer_view/customer_q_r_code_view/customer_q_r_code_view_widget.dart
@@ -0,0 +1,159 @@
+import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'dart:math';
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'customer_q_r_code_view_model.dart';
+export 'customer_q_r_code_view_model.dart';
+
+class CustomerQRCodeViewWidget extends StatefulWidget {
+  const CustomerQRCodeViewWidget({super.key});
+
+  @override
+  State<CustomerQRCodeViewWidget> createState() =>
+      _CustomerQRCodeViewWidgetState();
+}
+
+class _CustomerQRCodeViewWidgetState extends State<CustomerQRCodeViewWidget>
+    with TickerProviderStateMixin {
+  late CustomerQRCodeViewModel _model;
+
+  final animationsMap = <String, AnimationInfo>{};
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => CustomerQRCodeViewModel());
+
+    animationsMap.addAll({
+      'containerOnPageLoadAnimation': AnimationInfo(
+        trigger: AnimationTrigger.onPageLoad,
+        effectsBuilder: () => [
+          ScaleEffect(
+            curve: Curves.easeInOut,
+            delay: 0.0.ms,
+            duration: 300.0.ms,
+            begin: Offset(-5.0, -5.0),
+            end: Offset(1.0, 1.0),
+          ),
+        ],
+      ),
+    });
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    context.watch<FFAppState>();
+
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(32.0, 0.0, 32.0, 0.0),
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: FlutterFlowTheme.of(context).secondaryBackground,
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 8.0, 8.0, 0.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      InkWell(
+                        splashColor: Colors.transparent,
+                        focusColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        onTap: () async {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          Icons.close_rounded,
+                          color: FlutterFlowTheme.of(context).secondaryText,
+                          size: 24.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding:
+                      EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 32.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(
+                              0.0, 0.0, 0.0, 8.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  'แสดง QR Code ให้สมาชิกที่ต้องการเข้าร่วมองค์กร',
+                                  style: FlutterFlowTheme.of(context)
+                                      .bodyMedium
+                                      .override(
+                                        fontFamily: 'Kanit',
+                                        fontSize: 20.0,
+                                        letterSpacing: 0.0,
+                                      ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 32.0),
+                  child: BarcodeWidget(
+                    data: FFAppState().customerData.customerRef!.id,
+                    barcode: Barcode.qrCode(),
+                    width: 250.0,
+                    height: 250.0,
+                    color: FlutterFlowTheme.of(context).primaryText,
+                    backgroundColor: Colors.transparent,
+                    errorBuilder: (_context, _error) => SizedBox(
+                      width: 250.0,
+                      height: 250.0,
+                    ),
+                    drawText: false,
+                  ),
+                ),
+              ],
+            ),
+          ).animateOnPageLoad(animationsMap['containerOnPageLoadAnimation']!),
+        ),
+      ],
+    );
+  }
+}
