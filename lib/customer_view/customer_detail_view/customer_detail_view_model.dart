c712ad85f949e942e3a31ebde01d58292b3fb834
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-21 08:40:07 +0000

    Updating to latest FlutterFlow output.

--- lib/customer_view/customer_detail_view/customer_detail_view_model.dart
+++ lib/customer_view/customer_detail_view/customer_detail_view_model.dart
@@ -1,4 +1,5 @@
 import '/backend/backend.dart';
+import '/component/info_custom_view/info_custom_view_widget.dart';
 import '/customer_view/customer_q_r_code_view/customer_q_r_code_view_widget.dart';
 import '/customer_view/member_list_view/member_list_view_widget.dart';
 import '/customer_view/select_month_and_year_to_export_view/select_month_and_year_to_export_view_widget.dart';
@@ -7,6 +8,7 @@ import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'customer_detail_view_widget.dart' show CustomerDetailViewWidget;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:webviewx_plus/webviewx_plus.dart';
@@ -27,9 +29,44 @@ class CustomerDetailViewModel
           int index, Function(FFUploadedFile) updateFn) =>
       tmpPhotoList[index] = updateFn(tmpPhotoList[index]);
 
+  bool isExpire = true;
+
+  ///  State fields for stateful widgets in this component.
+
+  // Stores action output result for [Action Block - checkIsCustomerExpire] action in CustomerDetailView widget.
+  bool? checkIsExpire;
+
   @override
   void initState(BuildContext context) {}
 
   @override
   void dispose() {}
+
+  /// Action blocks.
+  Future<bool?> checkIsCustomerExpire(BuildContext context) async {
+    if (getCurrentTimestamp > widget!.customerDoc!.expireDate!) {
+      await showDialog(
+        context: context,
+        builder: (dialogContext) {
+          return Dialog(
+            elevation: 0,
+            insetPadding: EdgeInsets.zero,
+            backgroundColor: Colors.transparent,
+            alignment: AlignmentDirectional(0.0, 0.0)
+                .resolve(Directionality.of(context)),
+            child: WebViewAware(
+              child: InfoCustomViewWidget(
+                title: 'จำนวนวันใช้งานหมดแล้ว กรุณาต่ออายุการใช้งาน',
+                status: 'error',
+              ),
+            ),
+          );
+        },
+      );
+
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
