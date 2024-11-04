3e5d75fb8f4ee70701c27dedbeca11602d91a6aa
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-04 09:51:47 +0000

    Updating to latest FlutterFlow output.

--- lib/other_page/other_view/other_view_model.dart
+++ lib/other_page/other_view/other_view_model.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webviewx_plus/webviewx_plus.dart';
 
