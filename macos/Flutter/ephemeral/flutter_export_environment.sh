07c36eb6b38b0f7dd2bb0017b0fc37ebfca46e0f
darksilver99 <darksilver99@hotmail.com>
2024-11-22 11:41:19 +0700

    a

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/anusornkongthong/development/flutter3_24_2"
+export "FLUTTER_APPLICATION_PATH=/Users/anusornkongthong/development/flutter_project/work_time_check"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.3.4"
+export "FLUTTER_BUILD_NUMBER=35"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
